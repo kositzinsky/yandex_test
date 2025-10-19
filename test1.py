99de7c4bce1eabff422ddbe1a4411040b3bd55ec
kositzinsky <kositzinsky@gmail.com>
2025-10-19 10:15:25 +0500

    task 3

--- test1.py
+++ test1.py
@@ -1,33 +1,24 @@
-class MushroomsCollector:
-    def __init__(self, mushrooms=[]):
-        self.mushrooms = mushrooms
-    # Исправьте ошибку в этом методе.
-    def is_poisonous(self, mushroom_name):
-        return mushroom_name == 'Мухомор' or mushroom_name == 'Поганка'
-
-    # Допишите метод.
-    def add_mushroom(self, mushroom):
-        if self.is_poisonous(mushroom_name=mushroom):
-            print('Нельзя добавить ядовитый гриб')
-        else:
-            self.mushrooms.append(mushroom)
+class CipherMaster:
+    alphabet = 'абвгдеёжзийклмнопрстуфхцчшщъыьэюя'
     
-    # Напишите магический метод __str__,
-    # возвращающий перечень грибов из списка mushrooms 
-    # через запятую.
+    def cipher(self, original_text, shift):
+        # Метод должен возвращать зашифрованный текст 
+        # с учетом переданного смещения shift.
+        ...
+    
+    
+    def decipher(self, cipher_text, shift):
+        # Метод должен возвращать исходный текст
+        # с учётом переданного смещения shift.
+        ...
 
-    def __str__(self):
-        return ', '.join(self.mushrooms)
 
-
-# Пример запуска для самопроверки
-collector_1 = MushroomsCollector()
-collector_1.add_mushroom('Мухомор')
-collector_1.add_mushroom('Подосиновик')
-collector_1.add_mushroom('Белый')
-print(collector_1)
-
-collector_2 = MushroomsCollector()
-collector_2.add_mushroom('Лисичка')
-print(collector_1)
-print(collector_2)
+cipher_master = CipherMaster()
+print(cipher_master.cipher(
+    original_text='Однажды ревьюер принял проект с первого раза, с тех пор я его боюсь',
+    shift=2
+))
+print(cipher_master.decipher(
+    cipher_text='Олебэи яфвнэ мроплж сэжи — э пэй рдв злййвкпш лп нвящывнэ',
+    shift=-3
+))
