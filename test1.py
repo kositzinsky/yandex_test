7a06301690ae10c630dd16737dc71df8b8e8c1ce
kositzinsky <kositzinsky@gmail.com>
2025-10-19 11:01:55 +0500

    complete task 4

--- test1.py
+++ test1.py
@@ -1,23 +1,28 @@
 class CipherMaster:
     alphabet = 'абвгдеёжзийклмнопрстуфхцчшщъыьэюя'
-    
-    def cipher(self, original_text, shift):
-         for leter in original_text:
-             
-    
-    
-    def decipher(self, cipher_text, shift):
-        # Метод должен возвращать исходный текст
-        # с учётом переданного смещения shift.
-        ...
 
+    def process_text(self, text, shift, is_encrypt):
+        text = text.lower()
+        cipher_text = ''
+        if not is_encrypt:
+            shift = 0 - shift
+        for leter in text:
+            if leter not in CipherMaster.alphabet:
+                cipher_text += leter
+            else:
+                index = (CipherMaster.alphabet.find(leter) + shift) % 33
+                cipher_text += CipherMaster.alphabet[index]
+        return cipher_text
+        
 
 cipher_master = CipherMaster()
-print(cipher_master.cipher(
-    original_text='Однажды ревьюер принял проект с первого раза, с тех пор я его боюсь',
-    shift=2
+print(cipher_master.process_text(
+    text='Однажды ревьюер принял проект с первого раза, с тех пор я его боюсь',
+    shift=2,
+    is_encrypt=True
 ))
-print(cipher_master.decipher(
-    cipher_text='Олебэи яфвнэ мроплж сэжи — э пэй рдв злййвкпш лп нвящывнэ',
-    shift=-3
-))
+print(cipher_master.process_text(
+    text='Олебэи яфвнэ мроплж сэжи — э пэй рдв злййвкпш лп нвящывнэ',
+    shift=-3,
+    is_encrypt=False
+)) 
